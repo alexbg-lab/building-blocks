031fa27d4d2ec30d0a71584237a9d50fa184c3da
Alex Georgiev <sashsz@gmx.us>
2026-01-15 13:49:03 +0200

    move import statement

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,6 +1,6 @@
-if __name__ == "__main__":
-    import requests, os
+import requests, os
 
+if __name__ == "__main__":
     url: str | None = os.getenv("INPUT_URL")
 
     print("Status Code:", requests.get(url).status_code)
